f95760b299cc6a395744835dac8e2a76e4467459
HQNA-git <f.schubertHAW@web.de>
2022-04-30 19:05:27 +0200

    Escape script done
    Escape funktioniert Bugfrei soweit ich das beurteilen kann.

--- Software-Engineering/Assets/Scripts/Movement/Escape.cs
+++ Software-Engineering/Assets/Scripts/Movement/Escape.cs
@@ -6,35 +6,46 @@ using UnityEngine.AI;
 public class Escape : MonoBehaviour
 {
 
+    Hare hare;
     public NavMeshAgent _agent;
     public GameObject Fox;
+    public List<GameObject> foxList;
+   
+    public Vector3 _direction;
+    private float lowestDistance = 100;
+    private float _distanceToFox;
     public bool isFleeing = false;
-    private Vector3 _direction;
-
+    
+    
      
      public void start() {
+        hare = GetComponent<Hare>();
         _agent = GetComponent<NavMeshAgent>();
      }
      
      
      private void OnTriggerEnter(Collider col)
     {
-        if(col.tag == "Fox"){
-
-            // Den Fuchs in einen Array speichern
+        if(col.tag == "Fox")
+        {
             Fox = col.gameObject;
+            foxList.Add(Fox);
             isFleeing = true;
         }
     }
 
-    private void OnTriggerExit(Collider collider)
+    private void OnTriggerExit(Collider col)
     {
-
-         if(collider.gameObject.tag == "Fox"){
-            Fox = collider.gameObject;
+         if(col.gameObject.tag == "Fox")
+         {
+            
+            foxList.Remove(col.gameObject);
 
             //isFleeing erst auf false setzen wenn das array mit fuechsen leer ist
-            isFleeing = false;
+            if(foxList.Count == 0){
+                isFleeing = false;
+            }
+            
         }
     }
      public void Update() {
@@ -45,10 +56,40 @@ public class Escape : MonoBehaviour
      }
 
     
-    // wenn in einer Liste Gespeichert dann muss vor dem Fuchs geflohen werden der am dichtesten ist
+
     private void escape()
     {
-            // Distance to the fox
+        lowestDistance = 100;
+
+        Vector3 harePosition = transform.position;
+        Vector3 foxPosition = foxList[0].transform.position;
+
+        if(foxList.Count > 1){
+
+            // find the closest Fox
+            foreach(GameObject fox in foxList)
+            {
+                //Vector3 harePosition = transform.position;
+                foxPosition = fox.transform.position;
+                _distanceToFox = Vector3.Distance(harePosition, foxPosition);
+
+                if(_distanceToFox < lowestDistance){
+
+                    //Der Fox der am dichtesten ist wird zum gameObject Fox vor dem der Hase wegrennt
+                    Fox = fox;
+                    lowestDistance = _distanceToFox;
+                }
+            }
+
+           
+            _distanceToFox = Vector3.Distance(harePosition, foxPosition);
+            if(_distanceToFox < lowestDistance){
+
+                //Der Fox der am dichtesten ist wird zum gameObject Fox vor dem der Hase wegrennt
+                Fox = foxList[0];
+                lowestDistance = _distanceToFox;
+            }
+            
             Vector3 dirToFox = transform.position - Fox.transform.position;
             Debug.DrawLine(transform.position, Fox.transform.position, Color.red );
             
@@ -58,6 +99,19 @@ public class Escape : MonoBehaviour
 
             //Tell Agent where to go  
             _agent.SetDestination(_direction);
+            
+        }else{ // If there is only one Fox
+        
+            Vector3 dirToFox = transform.position - Fox.transform.position;
+            Debug.DrawLine(transform.position, Fox.transform.position, Color.red );
+            
+            // Escape direction
+            _direction = transform.position + (dirToFox).normalized;
+            Debug.DrawLine(transform.position,  _direction, Color.blue );
+
+            //Tell Agent where to go  
+            _agent.SetDestination(_direction);
+
+        }
     }
-   
 }
