0780fd304de5875fff2d55263558a5ecb2cc6076
SuILeY <soliman.anwari@haw-hamburg.de>
2022-05-02 11:46:45 +0200

    Menü (Sliderausarbeitung, noch nicht Spawn=

---
+++ Software-Engineering/Assets/SettingsmenuBunny.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+public class SettingsmenuBunny : MonoBehaviour
+{
+    public Slider slider;
+    public int bunny;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bunny = (int)slider.value;
+        
+    }
+}
---
+++ Software-Engineering/Assets/SettingsmenuFox.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingsmenuFox : MonoBehaviour
+{
+    public Slider slider;
+    public int fox=1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        fox = (int)slider.value;
+        Debug.Log(fox);
+    }
+}
