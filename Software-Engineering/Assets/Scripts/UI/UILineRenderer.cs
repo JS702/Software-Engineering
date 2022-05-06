929a02e187e43d5d61435e73263874cdb86f6cb4
JS702 <83334396+JS702@users.noreply.github.com>
2022-05-06 21:06:48 +0200

    amend to diagram renderer commit

--- Software-Engineering/Assets/Scripts/UI/UILineRenderer.cs
+++ Software-Engineering/Assets/Scripts/UI/UILineRenderer.cs
@@ -52,18 +52,18 @@ public class UILineRenderer : Graphic
         }
     }
     void DrawVerticesForPoint(Vector2 point, VertexHelper vh, float angle)
-        {
-            UIVertex vertex = UIVertex.simpleVert;
-            vertex.color = color;
+    {
+         UIVertex vertex = UIVertex.simpleVert;
+         vertex.color = color;
 
-            vertex.position = Quaternion.Euler(0,0,angle) * new Vector3(-thickness / 2, 0);
-            vertex.position += new Vector3(unitWidth * point.x , unitHeight * point.y);
-            vh.AddVert(vertex);
+         vertex.position = Quaternion.Euler(0,0,angle) * new Vector3(-thickness / 2, 0);
+         vertex.position += new Vector3(unitWidth * point.x , unitHeight * point.y);
+         vh.AddVert(vertex);
 
-            vertex.position = Quaternion.Euler(0, 0, angle) * new Vector3(thickness / 2, 0);
-            vertex.position += new Vector3(unitWidth * point.x, unitHeight * point.y);
-            vh.AddVert(vertex);
-        }
+         vertex.position = Quaternion.Euler(0, 0, angle) * new Vector3(thickness / 2, 0);
+         vertex.position += new Vector3(unitWidth * point.x, unitHeight * point.y);
+         vh.AddVert(vertex);
+    }
 
     public float GetAngle(Vector2 me, Vector2 target)
     {
@@ -81,85 +81,4 @@ public class UILineRenderer : Graphic
             }
         }
     }
-}
-
-/*
-
-public class UILineRenderer : Graphic
-{
-    public UIGridRenderer gridRenderer;
-
-    public Vector2Int gridSize;
-    public float thickness;
-
-    public List<Vector2> points;
-
-    float width;
-    float height;
-    float unitWidth;
-    float unitHeight;
-
-    protected override void OnPopulateMesh(VertexHelper vh)
-    {
-
-        vh.Clear();
-
-        width = rectTransform.rect.width;
-        height = rectTransform.rect.height;
-
-        unitWidth = width / gridSize.x;
-        unitHeight = height / gridSize.y;
-
-        if (points.Count < 2) return;
-
-
-        float angle = 0;
-        for (int i = 0; i < points.Count - 1; i++)
-        {
-
-            Vector2 point = points[i];
-            Vector2 point2 = points[i + 1];
-
-            if (i < points.Count - 1)
-            {
-                angle = GetAngle(points[i], points[i + 1]) + 90f;
-            }
-
-            DrawVerticesForPoint(point, point2, angle, vh);
-        }
-
-        for (int i = 0; i < points.Count - 1; i++)
-        {
-            int index = i * 4;
-            vh.AddTriangle(index + 0, index + 1, index + 2);
-            vh.AddTriangle(index + 1, index + 2, index + 3);
-        }
-    }
-    public float GetAngle(Vector2 me, Vector2 target)
-    {
-        //panel resolution go there in place of 9 and 16
-        return (float)(Mathf.Atan2(9f * (target.y - me.y), 16f * (target.x - me.x)) * Mathf.Rad2Deg);
-    }
-    void DrawVerticesForPoint(Vector2 point, Vector2 point2, float angle, VertexHelper vh)
-    {
-        UIVertex vertex = UIVertex.simpleVert;
-        vertex.color = color;
-
-        vertex.position = Quaternion.Euler(0, 0, angle) * new Vector3(-thickness / 2, 0);
-        vertex.position += new Vector3(unitWidth * point.x, unitHeight * point.y);
-        vh.AddVert(vertex);
-
-        vertex.position = Quaternion.Euler(0, 0, angle) * new Vector3(thickness / 2, 0);
-        vertex.position += new Vector3(unitWidth * point.x, unitHeight * point.y);
-        vh.AddVert(vertex);
-
-        vertex.position = Quaternion.Euler(0, 0, angle) * new Vector3(-thickness / 2, 0);
-        vertex.position += new Vector3(unitWidth * point2.x, unitHeight * point2.y);
-        vh.AddVert(vertex);
-
-        vertex.position = Quaternion.Euler(0, 0, angle) * new Vector3(thickness / 2, 0);
-        vertex.position += new Vector3(unitWidth * point2.x, unitHeight * point2.y);
-        vh.AddVert(vertex);
-    }
-}
-*/ 
+} 
