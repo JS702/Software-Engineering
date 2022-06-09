32733dd2c66ac719d43c2b5c212797ceea62615d
JS702 <83334396+JS702@users.noreply.github.com>
2022-06-09 19:57:18 +0200

    fixed aimdot missing

--- Software-Engineering/Assets/Scripts/UI/UIManager.cs
+++ Software-Engineering/Assets/Scripts/UI/UIManager.cs
@@ -28,7 +28,7 @@ public class UIManager : MonoBehaviour
             }
             else
             {
-                aimDot.SetActive(isActive);
+                aimDot.SetActive(!aimDot.activeSelf);
             }
 
         }
